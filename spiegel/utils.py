1fd2a0f2aa53681dbaba97ffb0a5d6939eb1a3e1
Michael Leue <domus@posteo.de>
2019-12-08 21:35:35 +0100

    add some TODOs

--- spiegel/utils.py
+++ spiegel/utils.py
@@ -1,3 +1,8 @@
+# TODO make this robust
+# TODO filter magic methods right here?
+# TODO write tests for this
+# TODO get all public attributes on class/object?
+
 def get_methods_on_class(cls):
     return [name for name, obj in vars(cls).items() if callable(obj)]
 
