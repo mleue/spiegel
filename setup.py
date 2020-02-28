0473701de67fbc8031a61dd7714a36fced1fc564
Michael Leue <domus@posteo.de>
2020-02-28 08:52:42 +0100

    release 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="spiegel",
-    version="0.1.1",
+    version="0.1.2",
     description="A server/client architecture for remote python objects.",
     license="MIT",
     packages=find_packages(exclude=["tests"]),
